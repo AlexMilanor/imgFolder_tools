8f5db6433fea6468a74346462e00d24e2f1d7d6b
AlexMilanor <3921872+AlexMilanor@users.noreply.github.com>
2025-01-31 22:33:09 -0300

    fix: Putting a non important output to db connection
    Instead of returning None, it is more interesting to return something,
    to help with debugging in the initial stages of the code.

--- imgFolder/db_connection.py
+++ imgFolder/db_connection.py
@@ -1,8 +1,11 @@
 
 
 class DBConn:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def query_files(self) -> dict:
-		pass
+    def query_files(self) -> dict:
+        return [
+            {"file":"file_1", "label":"label_1"},
+            {"file":"file_2", "label":"label_2"},
+        ]
