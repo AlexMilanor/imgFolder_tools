56016825cb2e39e9ca7cea1952dbd40851339fd3
AlexMilanor <3921872+AlexMilanor@users.noreply.github.com>
2025-02-14 20:48:26 -0300

    feat: FileTracker function to update file label

--- imgFolder/tracking.py
+++ imgFolder/tracking.py
@@ -5,6 +5,34 @@ class FileTracker:
 		self.folder = "."
 		self.db_client = DBConn()
 
-	def get_labels(self):
-		labels = [file['label'] for file in self.db_client.query_files()]
-		return labels
+		# List of dicts
+		self.current_files = self.db_client.query_files()
+
+
+	def get_all_labels(self) -> list:
+		labels = set()
+		for file in self.current_files:
+			labels.add(file['label'])
+
+		return sorted(labels)
+
+
+	def get_label(self, img_name):
+		label = None
+		for file in self.current_files:
+			if file['file'] == img_name:
+				label = file['label']
+
+		if label == None:
+			raise ValueError("No such file in the tracked list.")
+
+		return label
+
+
+	def set_label(self, img_name, label):
+		# ask for the db connection to update the file
+		self.db_client.update_db(
+						uid=img_name, 
+						field='label',
+						value=label
+						)
