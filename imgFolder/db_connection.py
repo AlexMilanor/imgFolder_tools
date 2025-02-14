98a244496bf4edd9e4d3212be6562fcf259e95a4
AlexMilanor <3921872+AlexMilanor@users.noreply.github.com>
2025-02-14 20:47:15 -0300

    feat: DBConn function to update the temp mock db

--- imgFolder/db_connection.py
+++ imgFolder/db_connection.py
@@ -1,11 +1,14 @@
 import csv
+from copy import copy
 
 class DBConn:
     def __init__(self):
-        self.file = "./db_prototype.csv"
+        self._file = "./db_prototype.csv"
+        self._schema = ['file', 'label']
+
 
     def query_files(self) -> dict:
-        with open(self.file, 'r') as csvfile:
+        with open(self._file, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
 
             files_list = []
@@ -13,3 +16,42 @@ class DBConn:
                 files_list.append(row)
         
         return files_list
+
+
+    def _change_file_list(
+            self, uid: str, field: str, value: str
+        ):
+
+        if field not in self._schema:
+            raise ValueError(f"Unknown field {field}")
+        
+        files_list = self.query_files()
+
+        new_files_list = []
+        for file in files_list:
+
+            row = copy(file)
+            if file['file'] == uid:
+                row[field] = value
+
+            new_files_list.append(row)
+
+        return new_files_list
+
+
+
+    def update_db(self, 
+                  uid: str, 
+                  field: str,
+                  value: str,
+        ) -> None:
+        
+        update_list = self._change_file_list(uid, field, value)
+
+        with open(self._file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self._schema)
+            writer.writeheader()
+
+            for row in update_list:
+                writer.writerow(row)
+
