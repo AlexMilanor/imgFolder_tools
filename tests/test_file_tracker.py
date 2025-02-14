5dc86a98729ac46189eec39a86657a649bcd7a0f
AlexMilanor <3921872+AlexMilanor@users.noreply.github.com>
2025-02-14 20:50:14 -0300

    test: Refactoring the FileTracker tests

--- tests/test_file_tracker.py
+++ tests/test_file_tracker.py
@@ -1,24 +1,31 @@
+import pytest
+
 from imgFolder.tracking import FileTracker
 from imgFolder.db_connection import DBConn
 
 
+
+# monkey patch DBConn.query_files
+@pytest.fixture
+def mock_response(monkeypatch):
+    def mock_query(self):
+        return [
+        {'file':'file_1', 'label':'comida'},
+        {'file':'file_2', 'label':'bebida'},
+        ]   
+
+    monkeypatch.setattr(DBConn, "query_files", mock_query)
+
+
 class TestFileTracker():
     
-    def test_get_labels_01(self, monkeypatch):
+    def test_get_labels_01(self, mock_response):
         # Arrange
-        # monkey patch DBConn.query_files
-        def mock_query(self):
-            return [
-            {'file':'file_1', 'label':'comida'},
-            {'file':'file_2', 'label':'bebida'},
-            ]
-
-        monkeypatch.setattr(DBConn, "query_files", mock_query)
         tracker = FileTracker()
         true = ["comida", "bebida"]
 
         # Act
-        res = tracker.get_labels()
+        res = tracker.get_all_labels()
         
         # Assert
         assert(true == res)
@@ -39,7 +46,29 @@ class TestFileTracker():
         true = ["móveis", "bebida"]
 
         # Act
-        res = tracker.get_labels()
+        res = tracker.get_all_labels()
         
         # Assert
         assert(true == res)
+
+
+    def test_get_label(self, mock_response):
+        # Arrange
+        tracker = FileTracker()
+        true = "bebida"
+
+        # Act
+        res = tracker.get_label("file_2")
+        
+        # Assert
+        assert(true == res)
+
+
+    def test_get_label_error(self, mock_response):
+        # Arrange
+        tracker = FileTracker()
+        
+        # Act
+        # Assert
+        with pytest.raises(ValueError):
+            res = tracker.get_label("file_3")
