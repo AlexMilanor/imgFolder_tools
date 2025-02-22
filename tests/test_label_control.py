afbe5bda4f4ac99ec92f15e438399cd9c1f37df9
AlexMilanor <3921872+AlexMilanor@users.noreply.github.com>
2025-02-21 23:02:20 -0300

    fix: Removing unneeded unit test for LabelControl
    The functions for LabelControl so far are just wrappers of File Tracker.
    No sense testing them again.

--- tests/test_label_control.py
+++ tests/test_label_control.py
@@ -1,15 +1,10 @@
+import pytest
+
 from imgFolder.commands import LabelControl
+from imgFolder.db_connection import DBConn
 
 
 class TestLabelControl():
 	
 	def test_prompt_label(self):
-		# Arrange
-		labeler = LabelControl(imgpath="Pudim")
-		true = {"Pudim":"Comida"}
-
-		# Act
-		res = labeler._set_label("Comida")
-		
-		# Assert
-		assert(true == res)
+		pass
